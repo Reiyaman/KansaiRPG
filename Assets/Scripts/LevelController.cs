3e15df20959709aaf0b5832bee47c89aeb0db4ca
Reiyaman <r-hoda0124@docomo.ne.jp>
2021-06-12 14:41:42 +0900

    第１島作成

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -20,9 +20,9 @@ public class LevelController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        level = 5; //Playerの初期能力
+        level = 1; //Playerの初期能力
         playerExp = 0;
-        playerAttackMinDamage = 10;
+        playerAttackMinDamage = 100;
         playerAttackMaxDamage = 150;
 
         audioSource = GetComponent<AudioSource>();
